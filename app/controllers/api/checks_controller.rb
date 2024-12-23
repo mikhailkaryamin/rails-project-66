8bdcec4dd38b8ecff277271ce69ae1238cd2dcaa
mikhailkaryamin <karyamin@uchi.ru>
2024-12-23 14:37:10 +0300

    complete step 6

---
+++ app/controllers/api/checks_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Api::ChecksController < Api::ApplicationController
+  def create
+    repository = Repository.find_by(github_id: params[:repository][:id])
+    check = repository.checks.create!
+    CheckReposJob.perform_later(check.id)
+    head :ok
+  end
+end
