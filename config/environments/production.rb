b366275073df21be6290e9f1e7f722ab254ca441
mikhailkaryamin <karyamin@uchi.ru>
2025-01-12 20:21:55 +0300

    add env default url to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  routes.default_url_options = { host: ENV.fetch('BASE_URL', nil) }
 end
