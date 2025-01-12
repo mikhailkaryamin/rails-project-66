730b47e6ce5a91fa7aec376da8090c20bb488f60
mikhailkaryamin <karyamin@uchi.ru>
2025-01-12 23:20:51 +0300

    fix from in ApplicationMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'mikhail.karyamin@uu.ru'
+  default from: 'MS_TOKHah@trial-3z0vklome7vg7qrx.mlsender.net'
   layout 'mailer'
 
   helper_method :asset_url
